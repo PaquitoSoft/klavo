71c46e564d871a9e22e6f67ffa2c0dad9a147233
PaquitoSoft <paquitosoftware@gmail.com>
2016-03-05 22:03:30 +0100

    Allow users to reset local storage with a query string parameter (reset)

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import lscache from 'lscache';
 
 import App from './components/layout/app';
+
+if (window.location.search.indexOf('reset') !== -1) {
+	lscache.flush();
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
