4708d77f5fb29a8012fb590f8c42f2e223f1681c
PaquitoSoft <paquitosoftware@gmail.com>
2016-03-05 19:43:34 +0100

    links from movies summaries to movies details

--- src/js/components/movie-summary.js
+++ src/js/components/movie-summary.js
@@ -5,12 +5,16 @@ export default function MovieSummary(props) {
 	return (
 		<div className="card movie-summary">
 			<div className="image">
-				<img src={movie.coverUrl} alt={movie.title} />
+				<a href={`/movie/${movie.cpId}`}>
+					<img src={movie.coverUrl} alt={movie.title} />
+				</a>
 			</div>
 			<div className="content movie-info">
-				<div className="header">{movie.title}</div>
+				<div className="header">
+					<a href={`/movie/${movie.cpId}`}>{movie.title}</a>
+				</div>
 				<div className="meta">
-					<a href="#">{movie.genre}</a>
+					<span>{movie.genre}</span>
 				</div>
 				<div className="description">
 				</div>
